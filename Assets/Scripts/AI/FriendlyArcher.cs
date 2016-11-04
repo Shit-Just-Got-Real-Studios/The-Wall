427933474bfcfafd3ba333a7c1e87d551cc199a2
l0fty-WhizZ <shreyas@shitjustgotreal.in>
2016-11-04 14:26:46 +0530

    Added Friendly AI archer logic.

---
+++ Assets/Scripts/AI/FriendlyArcher.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendlyArcher : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject bow;
+	private GameObject enemyTagged = null;
+
+	void SearchTarget () {
+		GameObject[] enemies = GameObject.FindGameObjectsWithTag ("ZombieBOT");
+		if (enemies.Length > 0) {
+			int randIndex = Random.Range (0, enemies.Length);
+			enemyTagged = enemies [randIndex];
+		}
+	}
+
+	void AttackEnemy () { 
+		transform.LookAt (enemyTagged.transform);
+		transform.rotation = Quaternion.Euler (0f, transform.localEulerAngles.y , 0f);
+		FriendlyArcherBowScript fab = bow.GetComponent<FriendlyArcherBowScript> ();
+		fab.Attack (enemyTagged);
+	}
+
+	void Awake () {
+		SearchTarget ();
+	}
+
+	void Start () {
+
+	}
+
+	void Update () {
+		if (enemyTagged == null)
+			SearchTarget ();
+		else if (enemyTagged.tag == "ZombieBOT")
+			AttackEnemy ();
+	}
+}
---
+++ Assets/Scripts/AI/FriendlyArcherBowScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FriendlyArcherBowScript : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject arrow;
+
+	[SerializeField]
+	private Transform spawnTransform;
+
+	public void Attack (GameObject enemyTagged) {
+		transform.LookAt (enemyTagged.transform);
+	}
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("ShootArrow",3,1);
+	}
+
+	void ShootArrow () {
+		Instantiate (arrow, spawnTransform.position, spawnTransform.rotation);
+	}
+
+}
